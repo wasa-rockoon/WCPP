e0559116a1b0bd46a225d38b456b4edc68f4ea47
Yuki Fujihara <mail@fujiy.dev>
2024-03-24 17:44:53 +0900

    不要ファイルを削除

--- cpp/CRC8.c
+++ cpp/CRC8.c
@@ -16,7 +16,7 @@
  *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -100,4 +100,5 @@ uint8_t update_crc_8( unsigned char crc, unsigned char val ) {
 
 	return sht75_crc_table[val ^ crc];
 
-}  /* update_crc_8 */
+} /* update_crc_8 */
+
