0def28c10febb064fec909553a2999abc55b9deb
fujiy <mail@fujiy.dev>
2024-07-25 17:09:07 +0900

    pythonのパッケージ設定

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="wcpp",
     version="1.0.0",
-    install_requires=[],
+    install_requires=['crc'],
     packages=['wcpp'],
     package_dir={'wcpp': 'python'}
 )
